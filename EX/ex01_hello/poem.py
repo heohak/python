867863213e35c92fdecc1e72a6899e8ce3bb3796
heohak <heohak@ttu.ee>
2022-09-01 08:58:38 +0300

    EX01 2osa

--- EX/ex01_hello/poem.py
+++ EX/ex01_hello/poem.py
@@ -12,10 +12,11 @@ And so will you!
 color = "red"
 objects = "violets"
 activity = "code"
-first_line = input("Which color are roses? ")
-second_line = input("Which flowers are blue? ")
-third_line = input("What do you like to do? ")
-print(f"Roses are {first_line},")
-print(f"{second_line} are blue,")
-print(f"I love to {third_line}")
+
+color = input("Which color are roses? ")
+objects = input("Which flowers are blue? ")
+activity = input("What do you like to do? ")
+print(f"Roses are {color},")
+print(f"{objects} are blue,")
+print(f"I love to {activity}")
 print("and so will you!")
