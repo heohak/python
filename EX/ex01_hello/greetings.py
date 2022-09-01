d45a53cfecea7b357d16002c1523c045c573cb0d
heohak <heohak@ttu.ee>
2022-09-01 09:39:13 +0300

    EX01 3osa

--- EX/ex01_hello/greetings.py
+++ EX/ex01_hello/greetings.py
@@ -12,5 +12,5 @@ Hello world! Hello world! Hello world!
 command1 = input("Enter a greeting: ")
 command2 = input("Enter a recipient: ")
 command3 = input("How many times to repeat: ")
-calculation = (command1 + " " + command2 + " ") * int(command3)
+calculation = (command1 + " " + command2 + "!" + " ") * int(command3)
 print(calculation)
