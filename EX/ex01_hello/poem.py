1743cf26de67ed874bcd8d86032e4b5984588889
heohak <heohak@ttu.ee>
2022-09-01 08:48:16 +0300

    EX01 2osa

--- EX/ex01_hello/poem.py
+++ EX/ex01_hello/poem.py
@@ -15,8 +15,7 @@ activity = "code"
 first_line = input("Which color are roses? ")
 second_line = input("Which flowers are blue? ")
 third_line = input("What do you like to do? ")
-
-print(f"Roses are {first_line },")
+print(f"Roses are {first_line},")
 print(f"{second_line} are blue,")
 print(f"I love to {third_line}")
 print("and so will you!")
