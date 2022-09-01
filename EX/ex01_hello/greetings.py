be0dfcffc61649358f78e3fb921a4f3ad8ce9f68
heohak <heohak@ttu.ee>
2022-09-01 09:27:50 +0300

    EX01 2osa

---
+++ EX/ex01_hello/greetings.py
@@ -0,0 +1,18 @@
+"""EX01 Greetings."""
+"""
+3. GreetingsGreetingsGreetings
+Example output:
+
+Enter a greeting: Hello
+Enter a recipient: world
+How many times to repeat: 3
+Hello world! Hello world! Hello world!
+
+"""
+greeting = "Hello"
+print(greeting)
+command1 = input("Enter a greeting: ")
+command2 = input("Enter a recipient: ")
+command3 = input("How many times to repeat: ")
+calculation = (command1 + " " + command2 + " ") * int(command3)
+print(calculation)
