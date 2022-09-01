938c2ef2923ae65cf57692e9a21bbacad7e30d70
heohak <heohak@ttu.ee>
2022-09-01 08:28:58 +0300

    EX01 2osa

---
+++ EX/ex01_hello/poem.py
@@ -0,0 +1,22 @@
+"""EX01 Poem."""
+"""
+2. Poem
+Example output:
+
+Roses are red,
+violets are blue,
+I love to code
+And so will you!
+
+"""
+color = "red"
+objects = "violets"
+activity = "code"
+first_line = input("Which color are roses? ")
+second_line = input("Which flowers are blue? ")
+third_line = input("What do you like to do? ")
+
+print(f"Roses are {first_line },")
+print(f"{second_line} are blue,")
+print(f"I love to {third_line}")
+print("and so will you!")
