2b52247b253650177ff9474ae04f85557e4b1287
heohak <heohak@ttu.ee>
2022-09-15 10:09:05 +0300

    EX03 osa 1 , katse 1

---
+++ EX/ex03_idcode/idcode.py
@@ -0,0 +1,93 @@
+"""EX03 ID code."""
+
+
+def find_id_code(text: str) -> str:
+    """
+    Find ID-code from given text.
+
+    Given string may include any number of numbers, characters and other symbols mixed together.
+    The numbers of ID-code may be between other symbols - they must be found and concatenated.
+    ID-code contains of exactly 11 numbers. If there are not enough numbers, return 'Not enough numbers!',
+    if there are too many numbers, return 'Too many numbers!' If ID-code can be found, return that code.
+    You don't have to validate the ID-code here. If it has 11 numbers, then it is enough for now.
+
+    :param text: string
+    :return: string
+    """
+    id_code = ""
+    for i in text:
+        if i.isdigit():
+            id_code += i
+    if len(id_code) > 11:
+        return "Too many numbers!"
+    elif len(id_code) < 11:
+        return "Not enough numbers!"
+    return id_code
+
+    # Write your code here
+
+
+def the_first_control_number_algorithm(text: str) -> str:
+    """
+    Check if given value is correct for control number in ID code only with the first algorithm.
+
+    The first algorithm can be calculated with ID code's first 10 numbers.
+    Each number must be multiplied with its corresponding digit
+    (in this task, corresponding digits are: 1 2 3 4 5 6 7 8 9 1), after which all the values are summarized
+    and divided by 11. The remainder of calculation should be the control number.
+
+    If the remainder is less than 10 and equal to the last number of ID code,
+    then that's the correct control number and the function should return the ID code.
+    Otherwise, the control number is either incorrect or the second algorithm should be used.
+    In this case, return "Needs the second algorithm!".
+
+    If the string contains more or less than 11 numbers, return "Incorrect ID code!".
+    In other case use the previous algorithm to get the code number out of the string
+    and find out, whether its control number is correct.
+
+    :param text: string
+    :return: string
+    """
+    id_code = ""
+    for i in text:
+        if i.isdigit():
+            id_code += i
+    if len(id_code) > 11:
+        return "Incorrect ID code!"
+    elif len(id_code) < 11:
+        return "Incorrect ID code!"
+
+    control_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+    sum_numbers = 0
+    new_id = id_code[:-1]
+    a = 0
+
+    for i in range(len(new_id)):
+        some_num = int(new_id[i])
+        sum_numbers += some_num * control_number[a]
+        a += 1
+    check_num = sum_numbers % 11
+    if check_num >= 10:
+        return "Needs the second algorithm!"
+    if check_num != int(id_code[-1]):
+        return "Incorrect ID code!"
+    if check_num == int(id_code[-1]):
+        return id_code
+
+    # Write your code here
+
+
+if __name__ == '__main__':
+    print("\nFind ID code:")
+    print(find_id_code(""))  # -> "Not enough numbers!"
+    print(find_id_code("123456789123456789"))  # -> "Too many numbers!"
+    print(find_id_code("ID code is: 49403136526"))  # -> "49403136526"
+    print(find_id_code("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
+
+    print(the_first_control_number_algorithm(""))  # -> "Incorrect ID code!"
+    print(the_first_control_number_algorithm("123456789123456789"))  # -> "Incorrect ID code!"
+    print(the_first_control_number_algorithm("ID code is: 49403136526"))  # -> "49403136526"
+    print(the_first_control_number_algorithm("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
+    print(the_first_control_number_algorithm("50412057633"))  # -> "50412057633"
+    print(the_first_control_number_algorithm("Peeter's ID is euf50weird2fs0fsk51ef6t0s2yr7fyf4"))  # -> "Needs
+    # the second algorithm!"
