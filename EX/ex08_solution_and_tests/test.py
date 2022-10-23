c16460a83f7950735156845923bc9a5fc3692c2e
heohak <heohak@ttu.ee>
2022-10-23 22:26:50 +0300

    EX08 tests

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -142,3 +142,4 @@ def test_fruit_order_not_enough():
 def test_enough_big_not_enough_smalls():
     """Not enough small baskets."""
     assert fruit_order(1, 8, 24) == -1
+    assert fruit_order(2, 1800, 1603) == -1
