4ed4523381a9a72c018e6158767d30af761e1573
heohak <heohak@ttu.ee>
2022-10-23 16:48:10 +0300

    EX08 tests

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -8,17 +8,16 @@ from solution import students_study
 def test_students_study_evening_coffee_not_needed():
     """During evening, coffee is not needed for studies."""
     assert students_study(18, True) is True
-    assert students_study(24, False) is True
-    assert students_study(20, True) is True
     assert students_study(21, False) is True
-    assert students_study(0, False) is False
+    assert students_study(-5, True) is False
 
 
 def test_students_study_night_no_study():
     """At night, students are sleeping."""
     assert students_study(1, True) is False
-    assert students_study(4, False) is False
     assert students_study(2, True) is False
-    assert students_study(3, False) is False
+
+
+
 
 
