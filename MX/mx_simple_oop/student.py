13867edc205032d3763e95a8299bd1844debc9dc
heohak <heohak@ttu.ee>
2022-11-02 10:38:21 +0200

    mx

---
+++ MX/mx_simple_oop/student.py
@@ -0,0 +1,9 @@
+"""Simple OOP Ex."""
+
+
+class Student:
+    """Class student."""
+    def __init__(self, name, finished=False):
+        """Constructor."""
+        self.name = name
+        self.finished = finished
