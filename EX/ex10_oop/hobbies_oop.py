ee655623623aa874a101d78fb93abec2ea144f73
heohak <heohak@ttu.ee>
2022-11-05 00:45:26 +0200

    ex10 osa2

--- EX/ex10_oop/hobbies_oop.py
+++ EX/ex10_oop/hobbies_oop.py
@@ -62,7 +62,7 @@ def sort_by_most_hobbies(people_list: list) -> list:
     :param people_list: list of people to sort.
     :return: sorted list of people.
     """
-    return sorted(people_list, key=lambda x: (len(x.hobbies), x.full_name), reverse=True)
+    return sorted(people_list, key=lambda x: (-len(x.hobbies), x.full_name))
 
 
 def sort_by_least_hobbies(people_list: list) -> list:
@@ -81,6 +81,7 @@ def sort_by_least_hobbies(people_list: list) -> list:
 def sort_people_and_hobbies(people_list: list) -> list:
     """
     Return a list of people but sorted alphabetically by their full name.
+
     Also sort their list of hobbies alphabetically.
 
     :param people_list: list of people to sort.
