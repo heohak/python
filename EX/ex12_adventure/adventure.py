a3390531234ebb53060c208db3fa8ea5d39d2a95
heohak <heohak@ttu.ee>
2022-11-17 18:06:41 +0200

    ex12

--- EX/ex12_adventure/adventure.py
+++ EX/ex12_adventure/adventure.py
@@ -29,15 +29,12 @@ class Adventurer:
 
     def add_experience(self, exp: int):
         """Increase experience."""
-        a = self.experience
-        self.experience += exp
-        if self.experience > 99:
-            self.power = self.power + (self.experience // 10)
-            self.experience = 0
-        if self.experience < 0:
-            if a > 0:
-                self.experience = a
-            self.experience = 0
+        if exp > 0:
+            self.experience += exp
+            if self.experience > 99:
+                self.power = self.power + (self.experience // 10)
+                self.experience = 0
+
 
 class Monster:
     """Adventurer opponent."""
